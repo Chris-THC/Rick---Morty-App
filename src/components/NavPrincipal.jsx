ed3a83727847d84e05a95dac29427c7c056ece13
Cris <chris_49git@outlook.com>
2022-08-05 19:43:27 -0500

    la app web funciona bien hata ahora

---
+++ src/components/NavPrincipal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Styles/NavPrincipal.css";
+const NavPrincipal = ({ texNav }) => {
+  return (
+    <nav>
+      <div>
+        <a href="/">{texNav.toUpperCase()}</a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavPrincipal;
